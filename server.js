0e072b5d222c6c8ea63a27d0a5d7e04442d5896c
danny50167 <danny.rollingsky@gmail.com>
2022-05-15 13:26:11 +0900

    엄
    준식

--- server.js
+++ server.js
@@ -6,22 +6,26 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home");
-  console.log("hello")
-})
+  console.log("hello");
+});
 
-app.get("/features", (req,res) => {
+app.get("/features", (req, res) => {
   const jsonData = require("./public/db/features.json");
 
-  res.render("features", {jsonData: jsonData});
-})
+  res.render("features", { jsonData: jsonData });
+});
 
-app.listen(3000, function() {
+app.get("/suggestions", (req, res) => {
+  res.render("suggestions");
+});
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
