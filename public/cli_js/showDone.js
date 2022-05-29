3d156874908ce153fa85bbb316c1a0bf65b1c6be
danny50167 <danny.rollingsky@gmail.com>
2022-05-29 10:03:39 +0900

    added favicon
    im so sexy

--- public/cli_js/showDone.js
+++ public/cli_js/showDone.js
@@ -1,11 +1,10 @@
 const btn = document.getElementById("btn_submit");
-const form = document.getElementById("form");
-
 btn.addEventListener("click", (event) => {
   event.preventDefault();
 
   setTimeout(() => {
-    form.style.display = "none";
-  }, 1000);
+    
+  }, 700);
+
   window.open("/")
 })
